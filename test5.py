57193607f83bff974ef0e15fe9fe55baa6b563a1
NormaPerson <156056257+NormaPerson@users.noreply.github.com>
2024-03-12 12:49:56 +0300

    Add files via upload

--- test5.py
+++ test5.py
@@ -1,5 +1,7 @@
 def hash(q, p=200, m=11800):
     # из-за другой кодировки, хоть и рекомендуется 118 но я решил взять большее число 11800
+    # функция кодирует в формате хэш отправленные ей строки
+    # функция возвращает 2 значения hash в десятичном формате и в hex
     hash_summ = 0
     for j, i in enumerate(q):
         hash_summ += ord(i) * p ** j
@@ -11,7 +13,9 @@ with open('game.txt', encoding='UTF-8') as f:
     # при записи в data очищаем строку от лишних смиволов (\n) и разделяем по символу $
     data = [i.strip().split('$') for i in data[1:]]
 
+# записываем в csv файл
 with open('game_with_hash.csv', 'w', encoding="UTF-8") as f:
     head = ", ".join(['hash'] + head.strip().split("$"))
+    # тк в условии не написанно как именно нужно записывать хэш, то, судя по формуле, требуют десятичный
     file_data = [', '.join([str(hash(''.join( i[0].split(' ') + i[1].split(' ') ))[0])] + i) for i in data]
     print(head + '\n' + '\n'.join(file_data), file=f)
